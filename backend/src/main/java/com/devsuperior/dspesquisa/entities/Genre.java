dbc993572f8b8895af4df3dc0d2e029b73ae85b8
Marcos Miranda <brunomiranda653@gmail.com>
2020-09-15 19:50:35 -0300

    POST, record

--- backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/entities/Genre.java
@@ -41,11 +41,11 @@ public class Genre implements Serializable {
 		this.id = id;
 	}
 
-	public String getNome() {
+	public String getName() {
 		return name;
 	}
 
-	public void setNome(String nome) {
+	public void setName(String nome) {
 		this.name = nome;
 	}
 
